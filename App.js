efb8c9c3275b7d4b6849c5b2c629f234b3bf0a28
Milena <milena.basso444@gmail.com>
2024-04-05 12:08:03 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [mensagem, setmensagem] = useState("");
+
+const atualizarMensagem("")
+
+changeTxt = (name) => {
+    setName(`Olá ${name}, bom dia. Tudo bom?`);
+  };
+
+  onChangeText={changeTxt}
+
+
+
+}
